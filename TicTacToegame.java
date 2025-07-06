c8790fa54bbd37857aa1ec6f2203ab75555379d3
Abivarna M <115912863+AbivarnaMasanam@users.noreply.github.com>
2025-07-06 17:32:27 +0530

    Update and rename game.java to TicTacToegame.java

--- TicTacToegame.java
+++ TicTacToegame.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class game {
+public class TicTacToegame {
+	//Board Design using array
     static char[][] board = {
         { ' ', ' ', ' ' },
         { ' ', ' ', ' ' },
@@ -9,10 +10,78 @@ public class game {
 
     static char currentPlayer = 'X';
 
+   
+    //Board layout printing 
+    static void printBoard() {
+        System.out.println("\n  1  2  3");
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print(" |");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("  ---------");
+        }
+        System.out.println();
+    }
+
+    //Checking for VaildMove
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    //Changing the player using Ternary Operator  
+    static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    //Checking for result (for winning)
+    static boolean checkWin() {
+        // Check rows, columns, and diagonals
+    	//rows
+        for (int i = 0; i < 3; i++)
+            if (board[i][0] == currentPlayer &&
+                board[i][1] == currentPlayer &&
+                board[i][2] == currentPlayer)
+                return true;
+
+        //columns
+        for (int i = 0; i < 3; i++)
+            if (board[0][i] == currentPlayer &&
+                board[1][i] == currentPlayer &&
+                board[2][i] == currentPlayer)
+                return true;
+
+        //Diagonals
+        if (board[0][0] == currentPlayer &&
+            board[1][1] == currentPlayer &&
+            board[2][2] == currentPlayer)
+            return true;
+
+        if (board[0][2] == currentPlayer &&
+            board[1][1] == currentPlayer &&
+            board[2][0] == currentPlayer)
+            return true;
+
+        return false;
+    }
+
+  //Checking for result (for Draw)
+    static boolean isDraw() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == ' ')
+                    return false;
+        return true;
+    }
+    
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean gameEnded = false;
 
+        //Logic of the Game
         while (!gameEnded) {
             printBoard();
             System.out.println("Player " + currentPlayer + ", enter your move (row and column: 1-3): ");
@@ -40,60 +109,4 @@ public class game {
         scanner.close();
     }
 
-    static void printBoard() {
-        System.out.println("\n  1 2 3");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((i + 1) + " ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j]);
-                if (j < 2) System.out.print("|");
-            }
-            System.out.println();
-            if (i < 2) System.out.println("  -----");
-        }
-        System.out.println();
-    }
-
-    static boolean isValidMove(int row, int col) {
-        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
-    }
-
-    static void switchPlayer() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-
-    static boolean checkWin() {
-        // Check rows, columns, and diagonals
-        for (int i = 0; i < 3; i++)
-            if (board[i][0] == currentPlayer &&
-                board[i][1] == currentPlayer &&
-                board[i][2] == currentPlayer)
-                return true;
-
-        for (int i = 0; i < 3; i++)
-            if (board[0][i] == currentPlayer &&
-                board[1][i] == currentPlayer &&
-                board[2][i] == currentPlayer)
-                return true;
-
-        if (board[0][0] == currentPlayer &&
-            board[1][1] == currentPlayer &&
-            board[2][2] == currentPlayer)
-            return true;
-
-        if (board[0][2] == currentPlayer &&
-            board[1][1] == currentPlayer &&
-            board[2][0] == currentPlayer)
-            return true;
-
-        return false;
-    }
-
-    static boolean isDraw() {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (board[i][j] == ' ')
-                    return false;
-        return true;
-    }
 }
